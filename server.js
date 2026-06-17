33cc10f30a47b5162ac65cfbca105ba2f3ffe5ad
Лия Николаева <l.nikolaeva@pampadu.ru>
2026-06-17 11:35:56 +0300

    add ask endpoint

--- server.js
+++ server.js
@@ -9,8 +9,18 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Таксопарк бот работает 🚕");
 });
+app.post("/ask", async (req, res) => {
 
-const PORT = 3000;
+  const question = req.body.question;
+
+  console.log("Вопрос:", question);
+
+  res.json({
+    answer: "Получил вопрос: " + question
+  });
+
+});
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
